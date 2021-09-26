31001b8a56bce39658593ea846d0848dee8ae640
Hinamoure <julia.debray@gmail.com>
2021-09-26 17:10:41 +0200

    Ajout d'une page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
+class HomeController extends AbstractController
 {
     /**
-     * @Route ("/", name="default_controller"),
+     * @Route("/home", name="home"),
      */
     public function index(): Response
     {
-        return $this->render('default/home.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return $this->render('home/home.html.twig', []);
     }
 }
